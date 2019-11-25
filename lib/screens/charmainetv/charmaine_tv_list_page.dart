571b9c920ffd5ecbbc0bb82eb01ca240a25d3bb5
TOURE NATHAN <toure21@live.com>
2019-11-26 01:55:05 +0300

    Merge the update of uitask branch into master

---
+++ lib/screens/charmainetv/charmaine_tv_list_page.dart
@@ -0,0 +1,69 @@
+import 'package:allthingscharmaine/model/charmainetv.dart';
+import 'package:allthingscharmaine/utils/custom_colors.dart';
+import 'package:allthingscharmaine/widgets/tourewidgets/charmaine_tv_item.dart';
+import 'package:allthingscharmaine/widgets/tourewidgets/drawer_widget.dart';
+import 'package:allthingscharmaine/screens/press/press_video_list_page.dart';
+import 'package:flutter/material.dart';
+
+class CharmaineTv extends StatefulWidget{
+
+  @override
+  State createState() => _CharmaineTvState();
+}
+
+class _CharmaineTvState extends State<CharmaineTv>{
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    List<Charmainetv> data = Data.getTVData();
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(icon: Image.asset('assets/hamb-menu.png'), onPressed: () { _scaffoldKey.currentState.openDrawer();}),
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: CustomColors.TITLE_COLOR,
+        ),
+        title: Text('charmaine tv' , style: TextStyle(fontWeight: FontWeight.w500, fontFamily: 'Poppins', fontSize:15.0, color: CustomColors.TITLE_COLOR),),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Image.asset('assets/notification.png'),
+            tooltip: 'Notification',
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: Drawer(child: NavigationDrawer(),),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 5.0,),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text('charmaine tv', style: TextStyle(color: CustomColors.TITLE_COLOR,
+                  fontFamily: 'Poppins', fontSize: 30.0, fontWeight: FontWeight.w600),),
+            ),
+            SizedBox(height: 10.0,),
+            ListView.builder(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0),
+              itemCount: data.length,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemBuilder: (context, index){
+                return GestureDetector(child: Container(child: CharmaineTvItem(imagePath: data[index].imagePath, name: data[index].name), padding: EdgeInsets.only(bottom: 10.0),),
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => PressVideoList(title: 'charmaine tv', category: data[index].name,)));
+                },);
+              },),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/tourewidgets/more_recent_articles.dart
@@ -0,0 +1,55 @@
+import 'package:allthingscharmaine/utils/custom_colors.dart';
+import 'package:allthingscharmaine/widgets/tourewidgets/press_article_item.dart';
+import 'package:allthingscharmaine/screens/press/press_article_list_page.dart';
+import 'package:flutter/material.dart';
+
+class MoreRecentArticles extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    List listData = Data.getArticleData();
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Row(children: [
+                    Padding(
+                        padding: EdgeInsets.only(right: 2.0),
+                        child: Text('most recent articles',
+                            style: TextStyle(
+                              color: CustomColors.TITLE_COLOR,
+                              fontSize: 20.0,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500,
+                            ))),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size: 12.0,
+                      color: CustomColors.TITLE_COLOR,
+                    )
+                  ]),
+                ),
+                Text('view all',
+                    style: TextStyle(
+                      color: CustomColors.TEXT_COLOR.withOpacity(0.5),
+                      fontSize: 12.0,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ))
+              ],
+            ),
+            SizedBox(
+              height: 19,
+            ),
+            GestureDetector(child: PressArticleItem(listData[0],),
+              onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> PressArticleList()));},),
+            SizedBox(height: 15, ),
+            GestureDetector(child: PressArticleItem(listData[1]),
+              onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> PressArticleList()));},),
+          ],
+        ));
+  }
+}
